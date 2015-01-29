12376a270987539ccb0ce3cba930b33f8fc8b8ca
vluna <vluna.0293@gmail.com>
2015-01-28 21:07:58 -0800

    added tip-calculator and currency-converter

---
+++ app/app/scripts/controllers/currency-converter.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name halloumiAppApp.controller:CurrencyConverterCtrl
+ * @description
+ * # CurrencyConverterCtrl
+ * Controller of the halloumiAppApp
+ */
+angular.module('halloumiAppApp')
+  .controller('CurrencyConverterCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
